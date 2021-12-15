8b81454345befe4efd37321ab2fb6d51e46d96cb
Kara Heeb <karaheeb@gmail.com>
2021-12-15 13:10:07 -0500

    starter code

---
+++ script.js
@@ -0,0 +1,12 @@
+// TODO Save reference to necessary DOM elements
+// What IDs and Classes were used on the modal form so we can capture the values?
+​
+// TODO How do we capture the current date in the following format Feb 21, 1998 at 01:32:30 pm
+    // What tools do we have to dynamically update the time every second?
+​
+// TODO What jQuery UI interactions/widgets will be used in this project?
+​
+// TODO Research how to implement tables in Bootstrap: https://getbootstrap.com/docs/4.0/content/tables/
+    // Can we create certain elements dynamically using jQuery based on user input?
+​
+// TODO What math will we need to calculate the Days Until Due Date and Potential Total Earnings columns?
